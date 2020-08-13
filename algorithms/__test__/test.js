bc1dd10ed496c47d548561bff8ad10721a00e6fd
laboratory <sipoohe@outlook.com>
2020-08-13 21:19:51 +0800

    no

--- algorithms/__test__/test.js
+++ algorithms/__test__/test.js
@@ -1,8 +1,13 @@
 let arr = [[1,1,2],[1,2],[3,3,4]];
-for(let i of arr[0]) {
-  for(let j of arr[1]) {
-    for(let k of arr[2]) {
-      console.log(i*100 + j*10 + k);
-    }
+
+function concat(num, i) {
+  if(i == arr.length) {
+    console.log(num);
+    return;
+  }
+  for(let k of arr[i]) {
+    concat(num * 10 + k, i+1);
   }
 }
+
+concat(0, 0);
