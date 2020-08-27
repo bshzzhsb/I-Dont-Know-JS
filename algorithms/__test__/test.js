9d3ea68f50b4ddb9b9e5a46c683b761c3ed4ffb5
sipoohe <1473427831@qq.com>
2020-08-28 00:34:22 +0800

    test:

--- algorithms/__test__/test.js
+++ algorithms/__test__/test.js
@@ -10,4 +10,32 @@ function concat(num, i) {
   }
 }
 
-concat(0, 0);
+// concat(0, 0);
+
+function partition(arr, left, right) {
+  let point = arr[right-1]
+  let minEnd = left;
+  let maxEnd;
+  for(maxEnd=left; maxEnd < right-1; maxEnd++) {
+    if(arr[maxEnd] - point < 0) {
+      let temp = arr[minEnd];
+      arr[minEnd] = arr[maxEnd];
+      arr[maxEnd] = temp;
+      minEnd++;
+    }
+  }
+  arr[right-1] = arr[minEnd];
+  arr[minEnd] = point;
+  return minEnd;
+}
+
+function quickSort(arr, left, right) {
+  if(left < right) {
+    let p = partition(arr, left, right);
+    quickSort(arr, left, p);
+    quickSort(arr, p+1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([56,312,123,59,30,50,78], 0, 7));
